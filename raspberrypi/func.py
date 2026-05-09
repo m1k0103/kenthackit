790de69b354c3b726168b0b9415c2172ba8e9597
mikolaj <m1k0103@therockman.tech>
2026-05-09 18:17:04 +0100

    trying to figure out a AsyncTimer class

--- raspberrypi/func.py
+++ raspberrypi/func.py
@@ -1,16 +1,70 @@
-def onMouseClick():
-    print(f"Mouse button pressed")
-    return True
-
+import asyncio
 
 class AsyncTimer:
-    def __init__(self):
-        self.currentTime = 0
-        self.status = "none"
+    def __init__(self, ms):
+        self.ms = 0
+        self.status = "done"
+        self.clicks = 0
+        self.loop = asyncio.new_event_loop()
+        self.task = None
 
-    def start(self, timems):
-        pass
 
-    def step(self, newTime):
-        pass
+    # Start the timer
+    async def start(self):
+        # if the timer is not running and still hasnt 
+        if (self.status == "done") & (self.ms != 0):
+            self.status == "running"
+
+            self.task = self.loop.create_task(self.step())
+            self.loop.run_forever()
+            
+        if self.status == "running":
+            print("is already running. do nothing")
+        
+        try:
+            self.loop.run_until_complete(self.task)
+            print("started loop")
+        finally:
+            return self.clicks
+
+
+
+    # Step. Decrease the amount of milliseconds remaining.
+    async def step(self):
+        #if self.ms > 0:
+        #    self.ms -= 1
+        #    return self.ms
+        #else:
+        #    return False
+        while status == "running":
+            if self.ms > 0:
+                self.ms -= 1
+                print(self.ms)
+                await asyncio.sleep(0.001) # Wait 1 ms. Check again if time has ran out.
+            else:
+                print("Timer has finished")
+                self.status = "done"
+                self.loop.stop()
+                done()
+        
+
+        # No more steps to do. No more time
+
+
+
+    # Increment click count 
+    async def click(self):
+        if self.ms > 0:
+            self.clicks += 1
+            print("click detected")
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
 
--- raspberrypi/start.py
+++ raspberrypi/start.py
@@ -1,16 +1,38 @@
 import requests
-from func import onMouseClick
+from func import AsyncTimer
+import asyncio
 
 
 
-def main():
-    print("start")
-    while True:
-        input()
-        onMouseClick()
+async def main():
+    print("started")
 
+    # create AsyncTimer object
+    timer = AsyncTimer(2000)
+    
+    async with asyncio.TaskGroup() as tg:
+        task = tg.create_task(timer.start())
+    print(f"Task execution complete. Result: {task.result}")
+
+    
+    #await timer.start()
+    #await timer.click()
+    #await timer.click()
+
+    quit()
+
+    # wait for an input (mouse click in the future)
+    #while True:
+    #    input("waiting")
+    #    timer.start()
+    #    timer.click()
+    #    print("clicked")
+        
+     
+        
 
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    asyncio.run(main())
