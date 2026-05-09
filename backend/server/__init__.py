4134ced83637203e9271aeed0bb10c130b602ce9
mikolaj <m1k0103@therockman.tech>
2026-05-09 12:51:15 +0100

    started on start script. added requirements

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,6 +1,24 @@
 from . import *
+import os
+
 
 
 def start():
     print("Started")
+
+
+    # 
+    from server.main import main
+    main()
     pass
+
+
+"""
+TODO:
+- make sure that the code checks if a database exists. if it doesnt, create a new database
+    - with the correct tables
+- 
+- 
+
+
+"""
--- backend/server/main.py
+++ backend/server/main.py
@@ -1,2 +1,4 @@
+import flask
+
 def main():
     pass
